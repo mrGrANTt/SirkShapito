69f1f8aec615401072deb1cd93adcdc9cbb18c13
mrGrANTt <sambistyyy0@gmail.com>
2025-03-31 21:15:38 +1000

    Image display

--- src/Hacker.java
+++ src/Hacker.java
@@ -1,6 +1,10 @@
-import java.io.BufferedReader;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Hacker {
@@ -10,16 +14,39 @@ public class Hacker {
     public static void main(String[] args) {
         try (
                 Socket sc = new Socket(ip, port);
-                BufferedReader in = new BufferedReader(new InputStreamReader(sc.getInputStream()));
         ) {
             System.out.println("Server connected. Waiting new msg...");
             while (true) {
-                String serverResponse = in.readLine();
-                if (serverResponse != null)
-                    System.out.println("Server msg: " + serverResponse);
+                byte[] imgByte = read(sc);
+                BufferedImage img = ImageIO.read(new ByteArrayInputStream(imgByte));
+
+                JFrame frame = new JFrame();
+                frame.getContentPane().setLayout(new FlowLayout());
+                frame.getContentPane().add(new JLabel(new ImageIcon(img)));
+                frame.pack();
+                frame.setVisible(true);
+
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             }
         } catch (IOException e) {
             System.out.println("Client error: " + e.getMessage());
         }
     }
+
+    public static byte[] read(Socket sc) throws IOException {
+        byte[] array = new byte[280 * 1024];
+        int arrayPointer = 0;
+        DataInputStream din = new DataInputStream(sc.getInputStream());
+        byte[] buffer = new byte[2048];
+        int readCount;
+
+        while ((readCount = din.read(buffer)) != -1) {
+            System.arraycopy(buffer, 0, array, arrayPointer, readCount);
+            arrayPointer += readCount;
+        }
+        return array;
+    }
 }
+
+
+// 211968b - 1920x1080 - ~207kb
